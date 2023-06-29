739972572514116290b6a5bc719bd5dada28c37e
ivanlopezmolina <ivanlopezmolina@gmail.com>
2023-06-29 14:41:56 -0400

    Create ClearSession.cs

---
+++ ClearSession.cs
@@ -0,0 +1,8 @@
+// Store the current session ID
+string sessionId = Session.SessionID;
+
+// Abandon the current session
+Session.Abandon();
+
+// Create a new session with the same session ID
+HttpContext.Current.SessionID = sessionId;
