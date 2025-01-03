18d78cb2f7420f2be413e643dc98fbe480d18484
ivanlopezmolina <ivanlopezmolina@gmail.com>
2025-01-03 14:38:20 -0500

    Update ObjectExtension.cs

--- ObjectExtension.cs
+++ ObjectExtension.cs
@@ -18,4 +18,41 @@ public static class ObjectExtensions
 }
 
 
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public static class ObjectExtensions
+{
+    public static Dictionary<string, string> ToDictionary(this object obj)
+    {
+        if (obj == null) throw new ArgumentNullException(nameof(obj));
+
+        var dictionary = new Dictionary<string, string>();
+        var properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var property in properties)
+        {
+            var value = property.GetValue(obj);
+            string formattedValue;
+
+            if (value is string stringValue)
+            {
+                // Wrap strings in single quotes
+                formattedValue = $"'{stringValue}'";
+            }
+            else
+            {
+                // Convert other types to string
+                formattedValue = value?.ToString() ?? "null";
+            }
+
+            dictionary.Add(property.Name, formattedValue);
+        }
+
+        return dictionary;
+    }
+}
+
+
  var dictionary = person.ToDictionary();
